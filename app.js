cca1f64cf1b318b7e34d3cb1e96bed445e3eed88
umnagendra <umnagendra@gmail.com>
2016-05-15 17:53:08 +0530

    Keeping all configs in a JSON file

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express     = require('express');
 var health      = require('express-ping');
 var bodyParser  = require('body-parser');
 var httpLogger  = require('morgan');
+var config      = require('./conf/config.json');
 
 // routers
 var index       = require('./routes/index');
 var fbWebhook   = require('./routes/fb-webhook');
 
-var port        = process.env.PORT || 7070;
+var port        = process.env.PORT || config.serverPort;
 var app         = express();
 
 app.use(bodyParser.json());
@@ -19,5 +20,5 @@ app.use('/', index);
 app.use('/webhook', fbWebhook);
 
 app.listen(port, function() {
-  console.log("Rosie listening on " + port);
+  console.log('Rosie listening on ' + port);
 });
