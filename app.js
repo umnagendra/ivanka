80b7fc634b074a56d845e8095cef443f3749311c
umnagendra <umnagendra@gmail.com>
2016-05-15 13:36:13 +0530

    Adding fb webhook API

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ var port        = process.env.PORT || 7070;
 // ping API
 app.use(health.ping());
 
+// webhook API
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === 'valar_morghulis') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
+
 app.listen(port, function() {
   console.log("Rosie listening on " + port);
 });
