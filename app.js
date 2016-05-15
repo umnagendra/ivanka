c9390553fbac7ae17ac335be26cd3c8fad2c98db
umnagendra <umnagendra@gmail.com>
2016-05-15 14:57:46 +0530

    added webhook to receive msgs from fb page

--- app.js
+++ app.js
@@ -7,13 +7,28 @@ var port        = process.env.PORT || 7070;
 // ping API
 app.use(health.ping());
 
-// webhook API
+// webhook token verify API
 app.get('/webhook', function (req, res) {
   if (req.query['hub.verify_token'] === 'valar_morghulis') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
 })
+
+// Receives messages on the fb page
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      console.log("Received text on fb page: [" + text + "]");
+      // TODO actual bot stuff
+    }
+  }
+  res.sendStatus(200);
+});
 
 app.listen(port, function() {
   console.log("Rosie listening on " + port);
