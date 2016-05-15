ff29945af6dbedb0440b966712fa6832e9019b73
umnagendra <umnagendra@gmail.com>
2016-05-15 15:36:59 +0530

    Rough plans on what TODO

--- app.js
+++ app.js
@@ -27,8 +27,15 @@ app.post('/webhook', function (req, res) {
     sender = event.sender.id;
     if (event.message && event.message.text) {
       text = event.message.text;
-      console.log("Received text on fb page: [" + text + "]");
-      // TODO actual bot stuff
+      console.log("RECEIVED: " + text);
+      // TODO - For every message:
+      // TODO - 1. Check if the sender is greeting you (hi, hello, hii, hey)
+      // TODO - 2. If greeting, greet him back, and ask him how you can help him
+      // TODO - 3. Then let him send his problem stmt - stem + tokenize, and match keywords
+      // TODO - If direct match with keywords, check if it falls under known FAQs and present answers
+      // TODO - Does that answer his question (ask)? If (no, not, really, kinda, kind of somewhat), then fwd request to SM
+      // TODO - If no direct match, say sorry and present a structured msg with problem stmts
+      // TODO - Get choice postback and send a request into SM and tell him to hold on
     }
   }
   res.sendStatus(200);
