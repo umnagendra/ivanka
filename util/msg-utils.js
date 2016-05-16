fdc82d653e3f220030a5d096a3e24742c9865b3e
umnagendra <umnagendra@gmail.com>
2016-05-16 10:53:35 +0530

    Removing temp debug logs

--- util/msg-utils.js
+++ util/msg-utils.js
@@ -5,7 +5,6 @@ var msgUtils = {};
 
 msgUtils.sendTextMessage = function(receiver, msgText) {
   console.log('Sending message [' + msgText + '] to ' + receiver + '...');
-  console.log('fb page access token is ' + config.fbPageAccessToken);
   msgData = {
     text : msgText
   };
@@ -18,10 +17,10 @@ msgUtils.sendTextMessage = function(receiver, msgText) {
                 message:    msgData,
             }
           }, function(error, response, body) {
-    if (error) {
-      console.error('Error sending message [' + msgText + '] to messenger: ', error);
-    } else if (response.body.error) {
-      console.log('Error sending message [' + msgText + '] to messenger: ', response.body.error);
+              if (error) {
+              console.error('Error sending message [' + msgText + '] to messenger: ', error);
+            } else if (response.body.error) {
+          console.log('Error sending message [' + msgText + '] to messenger: ', response.body.error);
     }
   });
 };
